e61da1038326aa0a907d7f53c46807c3b7732271
ebatuhan <batuhane2401@gmail.com>
2026-07-05 21:05:18 +0300

    double detach fixed

--- src/lib.rs
+++ src/lib.rs
@@ -39,129 +39,14 @@ impl<T> Tree<T> {
         self.root
     }
 
-    pub fn traverse(&self, root: NodeId) -> impl Iterator<Item = (NodeId, &T)> + '_ {
-        self.traverse_nodes(root)
-            .map(|(id, node)| (id, &node.value))
+    pub fn parent(&self, id: NodeId) -> Option<NodeId> {
+        self.get_node(id).and_then(|node| node.parent)
     }
 
-    pub fn traverse_ids(&self, root: NodeId) -> impl Iterator<Item = NodeId> + '_ {
-        self.traverse_nodes(root).map(|(id, _)| id)
-    }
-
-    fn traverse_nodes(&self, root: NodeId) -> TreeNodeTraversal<'_, T> {
-        TreeNodeTraversal {
-            stack: vec![root],
-            tree: self,
-        }
-    }
-
-    pub fn traverse_with_depth(
-        &self,
-        root: NodeId,
-    ) -> impl Iterator<Item = (NodeId, &T, usize)> + '_ {
-        self.traverse_nodes_with_depth(root)
-            .map(|(id, node, depth)| (id, &node.value, depth))
-    }
-
-    pub fn traverse_ids_with_depth(
-        &self,
-        root: NodeId,
-    ) -> impl Iterator<Item = (NodeId, usize)> + '_ {
-        self.traverse_nodes_with_depth(root)
-            .map(|(id, _, depth)| (id, depth))
-    }
-
-    fn traverse_nodes_with_depth(&self, root: NodeId) -> TreeNodeDepthTraversal<'_, T> {
-        TreeNodeDepthTraversal {
-            stack: vec![(root, 0)],
-            tree: self,
-        }
-    }
-
-    //TODO double detach will be fixed
-    pub fn insert_to(&mut self, value: T, parent_id: NodeId) -> NodeId {
-        let id = self.insert(value);
-
-        self.move_node(id, parent_id, Placement::In);
-
-        id
-    }
-
-    pub fn insert(&mut self, value: T) -> NodeId {
-        let id = {
-            if let Some(slot_id) = self.free_slots.pop() {
-                slot_id
-            } else {
-                NodeId::new(self.nodes.len())
-            }
-        };
-
-        let node = Node {
-            value,
-            parent: None,
-            children: Vec::new(),
-            generation: id.generation(),
-        };
-
-        if id.index() == self.nodes.len() {
-            self.nodes.push(Some(node));
-        } else {
-            self.nodes[id.index()] = Some(node);
-        }
-
-        self.move_node(id, self.root, Placement::In);
-
-        id
-    }
-
-    fn detach(&mut self, id: NodeId) {
-        self.nodes[id.index()] = None;
-        self.free_slots.push(id.next_gen());
-    }
-
-    pub fn remove_node(&mut self, root_id: NodeId) {
-        if root_id == self.root {
-            return;
-        }
-
-        let Some(node) = self.get_node(root_id) else {
-            return;
-        };
-
-        if let Some(parent_id) = node.parent {
-            self.get_node_mut(parent_id)
-                .unwrap()
-                .children
-                .retain(|child_id| *child_id != root_id);
-        }
-
-        let mut nodes_to_remove = Vec::<NodeId>::new();
-
-        for node_id in self.traverse_ids(root_id) {
-            nodes_to_remove.push(node_id);
-        }
-
-        for node_id in nodes_to_remove {
-            self.detach(node_id);
-        }
-    }
-
-    fn is_ancestor(&self, id: NodeId, mut target_id: NodeId) -> bool {
-        loop {
-            if id == target_id {
-                return true;
-            }
-
-            let Some(node) = self.get_node(target_id) else {
-                return false;
-            };
-
-            let Some(parent_id) = node.parent else {
-                return false;
-            };
-
-            target_id = parent_id
-        }
+    pub fn children(&self, id: NodeId) -> &[NodeId] {
+        self.get_node(id)
+            .map(|node| node.children.as_slice())
+            .unwrap_or(&[])
     }
 
     pub fn get(&self, id: NodeId) -> Option<&T> {
@@ -194,6 +79,74 @@ impl<T> Tree<T> {
             .filter(|node| node.generation == id.generation())
     }
 
+    pub fn insert(&mut self, value: T) -> NodeId {
+        let id = {
+            if let Some(slot_id) = self.free_slots.pop() {
+                slot_id
+            } else {
+                NodeId::new(self.nodes.len())
+            }
+        };
+
+        let node = Node {
+            value,
+            parent: None,
+            children: Vec::new(),
+            generation: id.generation(),
+        };
+
+        if id.index() == self.nodes.len() {
+            self.nodes.push(Some(node));
+        } else {
+            self.nodes[id.index()] = Some(node);
+        }
+
+        id
+    }
+
+    pub fn insert_to(&mut self, value: T, parent_id: NodeId) -> NodeId {
+        let id = self.insert(value);
+        self.move_node(id, parent_id, Placement::In);
+
+        id
+    }
+
+    pub fn insert_to_root(&mut self, value: T) -> NodeId {
+        self.insert_to(value, self.root)
+    }
+
+    pub fn remove_node(&mut self, root_id: NodeId) {
+        if root_id == self.root {
+            return;
+        }
+
+        let Some(node) = self.get_node(root_id) else {
+            return;
+        };
+
+        if let Some(parent_id) = node.parent {
+            self.get_node_mut(parent_id)
+                .unwrap()
+                .children
+                .retain(|child_id| *child_id != root_id);
+        }
+
+        let mut nodes_to_remove = Vec::<NodeId>::new();
+
+        for node_id in self.traverse_ids(root_id) {
+            nodes_to_remove.push(node_id);
+        }
+
+        for node_id in nodes_to_remove {
+            self.detach(node_id);
+        }
+    }
+
+    fn detach(&mut self, id: NodeId) {
+        self.nodes[id.index()] = None;
+        self.free_slots.push(id.next_gen());
+    }
+
     pub fn move_node(&mut self, id: NodeId, target_id: NodeId, placement: Placement) {
         match placement {
             Placement::In => {
@@ -205,6 +158,24 @@ impl<T> Tree<T> {
             Placement::After => {
                 self.place_node_next_to(id, target_id, 1);
             }
+        }
+    }
+
+    fn is_ancestor(&self, id: NodeId, mut target_id: NodeId) -> bool {
+        loop {
+            if id == target_id {
+                return true;
+            }
+
+            let Some(node) = self.get_node(target_id) else {
+                return false;
+            };
+
+            let Some(parent_id) = node.parent else {
+                return false;
+            };
+
+            target_id = parent_id
         }
     }
 
@@ -255,18 +226,6 @@ impl<T> Tree<T> {
         parent.children.push(id);
     }
 
-    pub fn contains(&self, id: NodeId) -> bool {
-        self.get_node(id).is_some()
-    }
-
-    fn find_child_index(&self, id: NodeId) -> Option<usize> {
-        let parent_id = self.parent(id)?;
-
-        self.children(parent_id)
-            .iter()
-            .position(|child_id| *child_id == id)
-    }
-
     fn detach_from_parent(&mut self, id: NodeId) {
         let Some(old_parent_id) = self.get_node(id).and_then(|node| node.parent) else {
             return;
@@ -281,31 +240,55 @@ impl<T> Tree<T> {
         }
     }
 
-    fn parent_node(&self, id: NodeId) -> Option<&Node<T>> {
-        self.get_node(id)
-            .and_then(|node| node.parent)
-            .and_then(|parent_id| self.get_node(parent_id))
+    pub fn contains(&self, id: NodeId) -> bool {
+        self.get_node(id).is_some()
     }
 
-    fn children_nodes(&self, id: NodeId) -> Vec<&Node<T>> {
-        self.get_node(id)
-            .map(|node| {
-                node.children
-                    .iter()
-                    .filter_map(|id| self.get_node(*id))
-                    .collect()
-            })
-            .unwrap_or_default()
+    fn find_child_index(&self, id: NodeId) -> Option<usize> {
+        let parent_id = self.parent(id)?;
+
+        self.children(parent_id)
+            .iter()
+            .position(|child_id| *child_id == id)
     }
 
-    pub fn parent(&self, id: NodeId) -> Option<NodeId> {
-        self.get_node(id).and_then(|node| node.parent)
+    pub fn traverse(&self, root: NodeId) -> impl Iterator<Item = (NodeId, &T)> + '_ {
+        self.traverse_nodes(root)
+            .map(|(id, node)| (id, &node.value))
     }
 
-    pub fn children(&self, id: NodeId) -> &[NodeId] {
-        self.get_node(id)
-            .map(|node| node.children.as_slice())
-            .unwrap_or(&[])
+    pub fn traverse_ids(&self, root: NodeId) -> impl Iterator<Item = NodeId> + '_ {
+        self.traverse_nodes(root).map(|(id, _)| id)
+    }
+
+    fn traverse_nodes(&self, root: NodeId) -> TreeNodeTraversal<'_, T> {
+        TreeNodeTraversal {
+            stack: vec![root],
+            tree: self,
+        }
+    }
+
+    pub fn traverse_with_depth(
+        &self,
+        root: NodeId,
+    ) -> impl Iterator<Item = (NodeId, &T, usize)> + '_ {
+        self.traverse_nodes_with_depth(root)
+            .map(|(id, node, depth)| (id, &node.value, depth))
+    }
+
+    pub fn traverse_ids_with_depth(
+        &self,
+        root: NodeId,
+    ) -> impl Iterator<Item = (NodeId, usize)> + '_ {
+        self.traverse_nodes_with_depth(root)
+            .map(|(id, _, depth)| (id, depth))
+    }
+
+    fn traverse_nodes_with_depth(&self, root: NodeId) -> TreeNodeDepthTraversal<'_, T> {
+        TreeNodeDepthTraversal {
+            stack: vec![(root, 0)],
+            tree: self,
+        }
     }
 }
 
